96b883f9836cc94932e52ee34a9c5a35e0311228
Nick <ndasto@protonmail.com>
2020-11-08 10:11:08 -0500

    refactored from CRA

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,41 @@
 import React from "react"
-import { Link } from "gatsby"
+import { createMuiTheme, ThemeProvider, Container } from "@material-ui/core"
+import Nav from "../components/nav"
+import Banner from "../components/banner"
+import Services from "../components/services"
+import About from "../components/about"
+import Contact from "../components/contact"
+import Forms from "../components/forms"
+import Footer from "../components/footer"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import "../styles/index.css"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#00e5bf",
+    },
+    secondary: {
+      main: "#00bbe6",
+    },
+    type: "dark",
+  },
+})
+
+const IndexPage = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="xl">
+        <Nav />
+        <Banner />
+        <Services />
+        <About />
+        <Contact />
+        <Forms />
+        <Footer />
+      </Container>
+    </ThemeProvider>
+  )
+}
 
 export default IndexPage
